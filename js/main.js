6bc7d4746ea5095fb46174b0e64dc6f3dbbadbc6
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-06-27 20:03:29 -0700

    refactored into es6 and made folders for file types

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var board = createJSBoard();
-var gamestate = resetGamestate();
+let board = createNewJSBoard();
+const gamestate = resetGameState();
 
-var $board = document.querySelector('.board');
+const $board = document.querySelector('.board');
 
 document.addEventListener('click', handleClick);
 
@@ -12,17 +12,17 @@ function createHTMLBoard() {
   $board.innerHTML = '';
   $board.applyClassToTile = applyClassToTile;
 
-  var coords = coordsArray();
+  const coords = coordsArray();
 
-  for (var i = 0; i < coords.length; i++) {
+  for (let i = 0; i < coords.length; i++) {
     // create a new row div at the start of each row
     if (coords[i] % 10 === 1) {
-      var $row = document.createElement('div');
+      const $row = document.createElement('div');
       $row.className = 'row board-row';
     }
 
     // append either a tile to the row
-    var $tile = document.createElement('div');
+    const $tile = document.createElement('div');
     $tile.className = 'tile';
     $tile.id = coords[i];
     $row.append($tile);
@@ -37,26 +37,26 @@ function createHTMLBoard() {
 function updateHTMLBoard() {
   createHTMLBoard();
 
-  var coords = coordsArray();
-  for (var i = 0; i < coords.length; i++) {
+  const coords = coordsArray();
+  for (let i = 0; i < coords.length; i++) {
     if (board[coords[i]]) {
-      var row = Math.floor(coords[i] / 10) - 1;
-      var col = Math.floor(coords[i] % 10) - 1;
+      const row = Math.floor(coords[i] / 10) - 1;
+      const col = Math.floor(coords[i] % 10) - 1;
 
       // append a div to display a chess piece
-      var $piece = document.createElement('div');
+      const $piece = document.createElement('div');
       $piece.classList.add('chess-piece', board[coords[i]]);
       $board.children[row].children[col].append($piece);
     }
   }
 }
 
-function createJSBoard() {
-  var coords = coordsArray();
-  var board = {};
+function createNewJSBoard() {
+  const coords = coordsArray();
+  const board = {};
 
   // add pieces to board
-  for (var i = 0; i < coords.length; i++) {
+  for (let i = 0; i < coords.length; i++) {
     if ((20 < coords[i] && coords[i] < 29) || (70 < coords[i] && coords[i] < 79)) {
       board[coords[i]] = 'p';
     } else if (coords[i] === 11 || coords[i] === 18 || coords[i] === 81 || coords[i] === 88) {
@@ -88,8 +88,8 @@ function createJSBoard() {
   return board;
 }
 
-function resetGamestate() {
-  var gamestate = {};
+function resetGameState() {
+  const gamestate = {};
   gamestate.turn = 'wb';
   gamestate.nextTurn = 'bw';
   gamestate.seeingOptions = false;
@@ -118,9 +118,9 @@ function resetGamestate() {
 }
 
 function coordsArray() {
-  var coords = [];
-  for (var i = 10; i < 90; i += 10) {
-    for (var j = 1; j < 9; j++) {
+  const coords = [];
+  for (let i = 10; i < 90; i += 10) {
+    for (let j = 1; j < 9; j++) {
       coords.push(i + j);
     }
   }
@@ -128,8 +128,8 @@ function coordsArray() {
 }
 
 function notACoord(number) {
-  var coords = coordsArray();
-  for (var i = 0; i < coords.length; i++) {
+  const coords = coordsArray();
+  for (let i = 0; i < coords.length; i++) {
     if (number === coords[i]) {
       return false;
     }
@@ -146,8 +146,8 @@ function colFromCoord(coord) {
 }
 
 function applyClassToTile(coord, cssClass) {
-  var row = rowFromCoord(coord);
-  var col = colFromCoord(coord);
+  const row = rowFromCoord(coord);
+  const col = colFromCoord(coord);
   this.children[row].children[col].classList.add(cssClass);
 }
 
@@ -158,7 +158,7 @@ function handleClick(event) {
     updateHTMLBoard();
     return;
   }
-  var coord = parseInt(event.target.closest('.tile').id);
+  const coord = parseInt(event.target.closest('.tile').id);
 
   if (gamestate.seeingOptions) {
     decideMove(coord);
@@ -168,8 +168,8 @@ function handleClick(event) {
 }
 
 function decideMove(end) {
-  var row = rowFromCoord(end);
-  var col = colFromCoord(end);
+  const row = rowFromCoord(end);
+  const col = colFromCoord(end);
   if (!$board.children[row].children[col].matches('.highlight')) {
     gamestate.seeingOptions = false;
     updateHTMLBoard();
@@ -209,22 +209,22 @@ function showOptions(start) {
   gamestate.seeingOptions = true;
 
   // find all potential moves
-  var potentialMoves = [];
-  var moveSpace = board.findMoveSpace(gamestate.turn, start, false);
-  for (var i = 0; i < moveSpace.length; i++) {
+  const potentialMoves = [];
+  const moveSpace = board.findMoveSpace(gamestate.turn, start, false);
+  for (const i = 0; i < moveSpace.length; i++) {
     if (isViableMove(gamestate.turn, start, moveSpace[i])) {
       potentialMoves.push(moveSpace[i]);
     }
   }
 
   // highlight all potential moves
-  for (var i = 0; i < potentialMoves.length; i++) {
+  for (let i = 0; i < potentialMoves.length; i++) {
     $board.applyClassToTile(potentialMoves[i], 'highlight');
   }
 }
 
 function findMoveSpace(turn, start, killsOnly) {
-  var piece = this[start][1];
+  const piece = this[start][1];
 
   if (piece === 'p') {
     return pawnMoveSpace(this, turn, start, killsOnly);
@@ -242,7 +242,7 @@ function findMoveSpace(turn, start, killsOnly) {
 }
 
 function pawnMoveSpace(board, turn, start, killsOnly) {
-  var moveSpace = [];
+  const moveSpace = [];
   if (board[start][0] === 'w') {
     // starting moves
     if (!killsOnly) {
@@ -254,9 +254,9 @@ function pawnMoveSpace(board, turn, start, killsOnly) {
       }
     }
     // attack moves
-    var pawnMoves = [9, 11];
-    for (var i = 0; i < pawnMoves.length; i++) {
-      var newSpot = start + pawnMoves[i];
+    const pawnMoves = [9, 11];
+    for (const i = 0; i < pawnMoves.length; i++) {
+      const newSpot = start + pawnMoves[i];
       if (notACoord(newSpot)) {
         continue;
       } else if (!board[newSpot]) {
@@ -278,9 +278,9 @@ function pawnMoveSpace(board, turn, start, killsOnly) {
       }
     }
     // attack moves
-    var pawnMoves = [-9, -11];
-    for (var i = 0; i < pawnMoves.length; i++) {
-      var newSpot = start + pawnMoves[i];
+    const pawnMoves = [-9, -11];
+    for (const i = 0; i < pawnMoves.length; i++) {
+      const newSpot = start + pawnMoves[i];
       if (notACoord(newSpot)) {
         continue;
       } else if (!board[newSpot]) {
@@ -296,12 +296,12 @@ function pawnMoveSpace(board, turn, start, killsOnly) {
 }
 
 function rookMoveSpace(board, turn, start) {
-  var moveSpace = [];
-  var rookMoves = [1, -1, 10, -10];
+  const moveSpace = [];
+  const rookMoves = [1, -1, 10, -10];
 
-  for (var i = 0; i < rookMoves.length; i++) {
-    for (var multiplier = 1; multiplier < 9; multiplier++) {
-      var newSpot = start + rookMoves[i] * multiplier;
+  for (const i = 0; i < rookMoves.length; i++) {
+    for (const multiplier = 1; multiplier < 9; multiplier++) {
+      const newSpot = start + rookMoves[i] * multiplier;
       if (notACoord(newSpot)) {
         break;
       } else if (!board[newSpot]) {
@@ -318,11 +318,11 @@ function rookMoveSpace(board, turn, start) {
 }
 
 function knightMoveSpace(board, turn, start) {
-  var moveSpace = [];
-  var knightMoves = [21, 12, -21, -12, 8, 19, -8, -19];
+  const moveSpace = [];
+  const knightMoves = [21, 12, -21, -12, 8, 19, -8, -19];
 
-  for (var i = 0; i < knightMoves.length; i++) {
-    var newSpot = start + knightMoves[i];
+  for (const i = 0; i < knightMoves.length; i++) {
+    const newSpot = start + knightMoves[i];
     if (notACoord(newSpot)) {
       continue;
     } else if (!board[newSpot]) {
@@ -337,12 +337,12 @@ function knightMoveSpace(board, turn, start) {
 }
 
 function bishopMoveSpace(board, turn, start) {
-  var moveSpace = [];
-  var bishopMoves = [11, -11, 9, -9];
+  const moveSpace = [];
+  const bishopMoves = [11, -11, 9, -9];
 
-  for (var i = 0; i < bishopMoves.length; i++) {
-    for (var multiplier = 1; multiplier < 9; multiplier++) {
-      var newSpot = start + bishopMoves[i] * multiplier;
+  for (const i = 0; i < bishopMoves.length; i++) {
+    for (const multiplier = 1; multiplier < 9; multiplier++) {
+      const newSpot = start + bishopMoves[i] * multiplier;
       if (notACoord(newSpot)) {
         break;
       } else if (!board[newSpot]) {
@@ -359,12 +359,12 @@ function bishopMoveSpace(board, turn, start) {
 }
 
 function queenMoveSpace(board, turn, start) {
-  var moveSpace = [];
-  var queenMoves = [1, -1, 10, -10, 11, -11, 9, -9];
+  const moveSpace = [];
+  const queenMoves = [1, -1, 10, -10, 11, -11, 9, -9];
 
-  for (var i = 0; i < queenMoves.length; i++) {
-    for (var multiplier = 1; multiplier < 9; multiplier++) {
-      var newSpot = start + queenMoves[i] * multiplier;
+  for (const i = 0; i < queenMoves.length; i++) {
+    for (const multiplier = 1; multiplier < 9; multiplier++) {
+      const newSpot = start + queenMoves[i] * multiplier;
       if (notACoord(newSpot)) {
         break;
       } else if (!board[newSpot]) {
@@ -381,11 +381,11 @@ function queenMoveSpace(board, turn, start) {
 }
 
 function kingMoveSpace(board, turn, start, killsOnly) {
-  var moveSpace = [];
-  var kingMoves = [10, -10, 1, -1, 11, -11, 9, -9];
+  const moveSpace = [];
+  const kingMoves = [10, -10, 1, -1, 11, -11, 9, -9];
 
-  for (var i = 0; i < kingMoves.length; i++) {
-    var newSpot = start + kingMoves[i];
+  for (const i = 0; i < kingMoves.length; i++) {
+    const newSpot = start + kingMoves[i];
     if (notACoord(newSpot)) {
       continue;
     } else if (!board[newSpot]) {
@@ -400,12 +400,12 @@ function kingMoveSpace(board, turn, start, killsOnly) {
 }
 
 function findEnemyMoveSpace(turn) {
-  var enemyMoveSpace = new Set();
-  var enemyCoord = [];
-  var coords = coordsArray();
+  const enemyMoveSpace = new Set();
+  const enemyCoord = [];
+  const coords = coordsArray();
 
   // find location of all enemy pieces
-  for (var i = 0; i < coords.length; i++) {
+  for (const i = 0; i < coords.length; i++) {
     if (!this[coords[i]]) {
       continue;
     } else if (this[coords[i]][0] === turn[1]) {
@@ -415,8 +415,8 @@ function findEnemyMoveSpace(turn) {
 
   // union all move spaces of enemy pieces
   for (i = 0; i < enemyCoord.length; i++) {
-    var eachMoveSpace = this.findMoveSpace(turn[1] + turn[0], enemyCoord[i], true);
-    for (var j = 0; j < eachMoveSpace.length; j++) {
+    const eachMoveSpace = this.findMoveSpace(turn[1] + turn[0], enemyCoord[i], true);
+    for (const j = 0; j < eachMoveSpace.length; j++) {
       enemyMoveSpace.add(eachMoveSpace[j]);
     }
   }
@@ -429,13 +429,13 @@ function isViableStart(start, turn) {
   }
 
   // find move space of start
-  var moveSpace = board.findMoveSpace(turn, start, false);
+  const moveSpace = board.findMoveSpace(turn, start, false);
   if (!moveSpace) {
     return false;
   }
 
   // is viable start if it has viable moves
-  for (var i = 0; i < moveSpace.length; i++) {
+  for (const i = 0; i < moveSpace.length; i++) {
     if (isViableMove(turn, start, moveSpace[i])) {
       return true;
     }
@@ -446,15 +446,15 @@ function isViableStart(start, turn) {
 }
 
 function isViableMove(turn, start, end) {
-  var potentialBoard = {...board};
+  const potentialBoard = {...board};
   potentialBoard.movePiece(start, end);
-  var enemyMoveSpace = potentialBoard.findEnemyMoveSpace(turn);
+  const enemyMoveSpace = potentialBoard.findEnemyMoveSpace(turn);
 
   // find ally king coord after move
-  var coords = coordsArray();
-  for (var i = 0; i < coords.length; i++) {
+  const coords = coordsArray();
+  for (const i = 0; i < coords.length; i++) {
     if (potentialBoard[coords[i]] === turn[0] + 'k') {
-      var kingCoord = coords[i];
+      const kingCoord = coords[i];
       break;
     }
   }
@@ -475,11 +475,11 @@ function movePiece(start, end) {
 }
 
 function checkmateScan() {
-  var enemyCoords = [];
+  const enemyCoords = [];
 
   // find location of all enemies
-  var coords = coordsArray();
-  for (var i = 0; i < coords.length; i++) {
+  const coords = coordsArray();
+  for (const i = 0; i < coords.length; i++) {
     if (board[coords[i]]) {
       if (board[coords[i]][0] === gamestate.turn[1]) {
         enemyCoords.push(coords[i]);
